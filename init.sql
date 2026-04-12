af3777668040ba4ce0bae690d2bbd9a25b3854fc
Ağa Ağalı <agaagali633.19@gmail.com>
2026-04-13 02:08:22 +0400

    add init.sql

---
+++ init.sql
@@ -0,0 +1,7 @@
+﻿CREATE DATABASE users_db;
+CREATE DATABASE test_db;
+CREATE DATABASE learning_db;
+CREATE DATABASE notification_db;
+CREATE DATABASE statistics_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE permission_db;
